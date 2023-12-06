7253dcb9a12ecef569da999dc08eb8b3224858bc
alyokaz <alyoshakaz@live.co.uk>
2023-12-06 23:55:54 +0000

    Add database population test

--- src/test/java/copper/co/test/ExampleApplicationTest.java
+++ src/test/java/copper/co/test/ExampleApplicationTest.java
@@ -1,12 +1,23 @@
 package copper.co.test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ExampleApplicationTest {
 
+	@Autowired
+	ExampleEntityRepository repository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void databaseIsPopulated() {
+		assertThat(repository.findAll().size() == 30);
 	}
 }
